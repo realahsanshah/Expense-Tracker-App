a9dbd96295c826afebd9f0f4f68573d10fee2738
ahsanshah <shahahsan56@gmail.com>
2020-08-08 02:13:26 +0500

    Added History Component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './components/Header';
 import Balance from './components/Balance';
 import IncomeExpense from './components/IncomeExpense';
+import History from './components/History';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <div className='container'>
         <Balance />
         <IncomeExpense />
+        <History />
       </div>
     </div>
   );
---
+++ src/components/History.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+const History = () => {
+    return ( 
+        <div>
+            <h4>History</h4>
+            <ul className='history-list'>
+
+            </ul>
+        </div>
+     );
+}
+ 
+export default History;
