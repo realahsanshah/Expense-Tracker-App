d9e63f8c2ab643a9e2830f9629c069cf1822de14
ahsanshah <shahahsan56@gmail.com>
2020-08-08 03:03:30 +0500

    Deleted Dispatch for now

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -16,7 +16,7 @@ export const GlobalContext=createContext(initialState);
 
 
 export const GlobalProvider=({children})=>{
-    const [state,dispatch]=useReducer(AppReducer,initialState);
+    const [state]=useReducer(AppReducer,initialState);
 
     return (<GlobalContext.Provider value={{
         transactions:state.transactions
