eef3fa9c905735b0cc5783ee143c5da4ebfa876c
ahsanshah <shahahsan56@gmail.com>
2020-08-08 02:10:35 +0500

    Added Income Expense Component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import Balance from './components/Balance';
+import IncomeExpense from './components/IncomeExpense';
 
 function App() {
   return (
     <div>
       <Header />
-      <Balance />
+      <div className='container'>
+        <Balance />
+        <IncomeExpense />
+      </div>
     </div>
   );
 }
