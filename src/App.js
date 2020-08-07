c390e8559573b627c9e865d1666c764765a6989c
ahsanshah <shahahsan56@gmail.com>
2020-08-08 02:07:22 +0500

    Added Balance

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import Balance from './components/Balance';
 
 function App() {
   return (
     <div>
       <Header />
+      <Balance />
     </div>
   );
 }
