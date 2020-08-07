6cc3df2006613b421622305e6abb87a3425ebb51
ahsanshah <shahahsan56@gmail.com>
2020-08-08 02:03:54 +0500

    Added Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div className="App">
-      <h2>Expense Tracker App</h2>
+    <div>
+      <Header />
     </div>
   );
 }
