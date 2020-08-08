0915a3d76cb0fa344052f94cc2367f621a0cdb6b
ahsanshah <shahahsan56@gmail.com>
2020-08-08 12:47:42 +0500

    Income Expense Added

--- src/components/IncomeExpense.js
+++ src/components/IncomeExpense.js
@@ -1,15 +1,21 @@
-import React from 'react';
+import React,{useContext} from 'react';
+import {GlobalContext} from '../context/GlobalState';
 
 const IncomeExpense = () => {
+  const {transactions} = useContext(GlobalContext);
+
+    const amounts = transactions.map(trans=>trans.amount);
+    const income = amounts.filter(item=>item>0).reduce((acc,item)=>(acc+=item),0).toFixed(2);
+    const expense = amounts.filter(item=>item<0).reduce((acc,item)=>(acc+=Math.abs(item)),0).toFixed(2);
     return ( 
         <div class="inc-exp-container">
         <div>
           <h4>Income</h4>
-          <p id="money-plus" className="money plus">+$0.00</p>
+          <p id="money-plus" className="money plus">+${income}</p>
         </div>
         <div>
           <h4>Expense</h4>
-          <p id="money-minus" className="money minus">-$0.00</p>
+    <p id="money-minus" className="money minus">-${expense}</p>
         </div>
       </div>
      );
