f8f60165210a47fc9bf1d12228b586e87c7f1b78
ahsanshah <shahahsan56@gmail.com>
2020-08-08 12:43:09 +0500

    Calculated Balance

--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -1,5 +1,12 @@
 export default (state,action)=>{
     switch(action.type){
+        
+        case 'ADD_TRANSACTION':
+            return state;
+
+        case 'REMOVE_TRANSACTION':
+            return state;
+
         default:
             return state;
     }
